3533c996e39adb793bd2b782b01f33863874b074
Reyhaneh Ozgoli <rozgoli@vpnb-003.uni-duisburg.de>
2025-01-12 15:58:37 +0100

    Seite flowchart-taskund gap-task

--- angularApp/src/app/tasks-form/flowchart-task/flowchart-task.component.ts
+++ angularApp/src/app/tasks-form/flowchart-task/flowchart-task.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { FormsModule, NgModel } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+declare var Prism: any; // To use Prism globally
 
 @Component({
   selector: 'app-flowchart-task',
@@ -7,75 +12,79 @@ import { Component } from '@angular/core';
   templateUrl: './flowchart-task.component.html',
   styleUrl: './flowchart-task.component.sass'
 })
-export class FlowchartTaskComponent {
+
+export class FlowchartTaskComponent implements OnInit, AfterViewInit{
+  taskName: string = ''; // Dynamischer Task-Name
+  errorMessage: string = '';
+
+  type: string = ''
+  category: string = ''
+  skillLevel: string = ''
+
+  constructor(private taskService: TaskService,
+    private router: ActivatedRoute,
+    private httpClient: HttpClient
+  ) {
+    taskService.data$.subscribe((data) => {
+      this.type = data.type
+      this.category = data.category
+      this.skillLevel = data.skill
+    })
+
+    this.router.queryParams.subscribe((params) => {
+      this.type = params['type']
+      this.category = params['category']
+      this.skillLevel = params['skill']
+    })
+  }
+  ngAfterViewInit(): void {
+    // Apply syntax highlighting after the view is initialized
+    Prism.highlightAll();
+  }
+
+  ngOnInit(): void {
+    this.generateTaskTitle();
+  }
+
+  generateTaskTitle(): void {
+    this.taskService.generateTaskTitle(this.category, this.type).subscribe({
+      next: (response: { status: string; title: string; message?: string }) => {
+        if (response.status === 'success') {
+          this.taskName = response.title;
+        } else {
+          this.errorMessage = response.message || 'Error generating task name.';
+        }
+      },
+      error: (err: any) => {
+        this.errorMessage = 'Error fetching task name.';
+        console.error(err);
+      },
+    });
+  }
+  
+  save(){
+    alert('saving')
+  }
+
+  create()
+  {
+    let baseUrl = "http://127.0.0.1:5000/";
+
+    let headers = new HttpHeaders({
+      Authorization: 'Bearer ' + localStorage.getItem('token')
+    });
+
+    this.httpClient.post(baseUrl + 'task', {
+      'difficultyLevel': this.skillLevel,
+      'topic' : this.category,
+      'type': this.type
+    }, 
+    {'headers': headers}
+  ).subscribe((response) => {
+      console.log(response)
+    })
+
+  }
 
 }
-// import { Component, OnInit } from '@angular/core';
-// import { TaskService } from '../../services/task.service';
-// import { FormsModule, NgModel } from '@angular/forms';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-
-// @Component({
-//   selector: 'app-compiler-task',
-//   standalone: true,
-//   imports: [FormsModule],
-//   templateUrl: './compiler-task.component.html',
-//   styleUrl: './compiler-task.component.sass'
-// })
-// export class CompilerTaskComponent implements OnInit{
-//   taskName: string = ''; // Für den Task-Namen
-//   errorMessage: string = '';
-
-//   type: string = ''
-//   category: string = ''
-//   skillLevel: string = ''
-
-//   constructor(private taskService: TaskService,
-//     private router: ActivatedRoute,
-//     private httpClient: HttpClient
-//   ) {
-//     taskService.data$.subscribe((data) => {
-//       this.type = data.type
-//       this.category = data.category
-//       this.skillLevel = data.skill
-//     })
-
-//     this.router.queryParams.subscribe((params) => {
-//       this.type = params['type']
-//       this.category = params['category']
-//       this.skillLevel = params['skill']
-//     })
-//   }
-
-//   ngOnInit(): void {}
-
-  
-  
-//   save(){
-//     alert('saving')
-//   }
-
-//   create()
-//   {
-//     let baseUrl = "http://127.0.0.1:5000/";
-
-//     let headers = new HttpHeaders({
-//       Authorization: 'Bearer ' + localStorage.getItem('token')
-//     });
-
-//     this.httpClient.post(baseUrl + 'task', {
-//       'difficultyLevel': this.skillLevel,
-//       'topic' : this.category,
-//       'type': this.type
-//     }, 
-//     {'headers': headers}
-//   ).subscribe((response) => {
-//       console.log(response)
-//     })
-
-//   }
-
-// }
 
