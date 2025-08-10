94440acba6a1b3618754bbffab232fe3dbc93bb2
simonnkata <simonnkata52@gmail.com>
2025-08-10 19:12:21 +0200

    Implement student and task registration routes; refactor teacher routes

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,8 @@ from flask_bcrypt import Bcrypt
 from datetime import datetime, timedelta
 from flask_jwt_extended import create_access_token, JWTManager, jwt_required, get_jwt_identity
 from teacher import register_teacher_routes
+from backend.task import register_task_routes
+from student import register_student_routes
 from feedback import feedback_routes
 app = Flask(__name__)
 bcrypt= Bcrypt(app)
@@ -20,147 +22,7 @@ client = MongoClient("mongodb://mongo:27017/IPT_db")
 db = client["IPT_db"]
 register_teacher_routes(app, db, bcrypt, jwt)
 feedback_routes(app, db, bcrypt, jwt)
-
-@app.route('/hello')
-def hello():
-    return "Hello World!"
-
-@app.route('/student_signup', methods=['POST'])
-def save_user():
-    code = 500
-    res_data = {}
-    message = ""
-    status = "fail"
-    try:
-        data = request.get_json()
-        username=data['username']
-        if db.users.find_one({'username':username}):
-            message = "user with that username already exists"
-            code = 401
-            status = "fail"
-        else:
-            # hashing the password so it's not stored in the db as it was
-            data['password'] = bcrypt.generate_password_hash(data['password']).decode('utf-8')
-            data["experienceLevel"]="beginner"
-            data["solvedTasks"]=[]
-            data['UserCreated'] = datetime.now()
-            access_token=create_access_token(identity=username)
-            res = db.users.insert_one(data)
-            if res.acknowledged:
-                status = "successful"
-                message = "user created successfully"
-                code = 200
-                res_data={"username":username, "token":access_token, "experienceLevel":data["experienceLevel"],"solvedTasks":data["solvedTasks"]}
-    except Exception as ex:
-        message = f"{ex}"
-        status = "fail"
-        code = 500    
-    return jsonify({'status': status, "data": res_data, "message":message}), code
-
-@app.route('/student_login', methods=['POST'])
-def login():
-    message = ""
-    res_data = {}
-    code = 500
-    status = "fail"
-    try:
-        data = request.get_json()
-        username=data['username']
-        user = db.users.find_one({'username':username})
-        print(user)
-        if user:
-            passwordhashed=user["password"]
-            password=data["password"]
-            experienceLevel = user["experienceLevel"]
-            solvedTasksList = user["solvedTasks"]
-            if user and bcrypt.check_password_hash(passwordhashed, password):
-                access_token=create_access_token(identity=username)
-                message = "user authenticated"
-                code = 200
-                status = "successful"
-                res_data={"username":username, "token":access_token, "experienceLevel":experienceLevel,"solvedTasks":solvedTasksList}
-                print(res_data)
-
-            else:
-                message = "wrong password"
-                code = 401
-                status = "fail"
-        else:
-            message = "invalid login details"
-            code = 401
-            status = "fail"
-
-    except Exception as ex:
-        message = f"{ex}"
-        code = 500
-        status = "fail"
-    return jsonify({'status': status, "data": res_data, "message":message}), code
-
-@app.route('/upload_logged_data', methods=['POST'])
-@jwt_required()
-def upload_logged_data():
-    try:
-        data = request.json
-        current_user=get_jwt_identity()
-        print(current_user)
-        print(data["username"])
-        result = db.IPT_logs.insert_one(data)
-        return jsonify({"success": True, "message": "Data uploaded successfully", "id": str(result.inserted_id)})
-    except Exception as e:
-        return jsonify({"success": False, "message": str(e)})
-
-@app.route('/changeExpLevel', methods=['POST'])
-@jwt_required()
-def changeExpLevel():
-    message = ""
-    res_data = {}
-    code = 500
-    status = "fail"
-    print(".")
-    try:
-        data = request.get_json()
-        current_user=get_jwt_identity()
-        newExpLevel=data["experienceLevel"]
-        filter = {'username': current_user}
-        newvalues = { "$set": { 'experienceLevel': newExpLevel } }
-        db.users.update_one(filter, newvalues)
-        code=200
-        status="successful"
-        res_data={"user_id":current_user}
-    except Exception as ex:
-        message = f"{ex}"
-        code = 500
-        status = "fail"
-    return jsonify({'status': status, "data": res_data, "message":message}), code
-
-@app.route('/updateSolvedTasks', methods=['POST'])
-@jwt_required()
-def updateSolvedTasks():
-    message = ""
-    res_data = {}
-    code = 500
-    status = "fail"
-    try:
-        print(request)
-        data= request.get_json()
-        print("Hello")
-        current_user=get_jwt_identity()
-        print("works")
-        filter = {'username': current_user}
-        task=data["taskID"]
-        print(task)
-        newvalues = { "$push": { 'solvedTasks': task} }
-        db.users.update_one(filter, newvalues)
-        code=200
-        status="successful"
-        res_data={"user_id":current_user}
-    except Exception as ex:
-        print(ex)
-        message = f"{ex}"
-        code = 500
-        status = "fail"
-    return jsonify({'status': status, "data": res_data, "message":message}), code
-
-
+register_task_routes(app, db, bcrypt, jwt)
+register_student_routes(app, db, bcrypt, jwt)
 if __name__=='__main__':
         app.run(host='0.0.0.0', port=5001)
---
+++ backend/task.py
@@ -0,0 +1,201 @@
+from flask import jsonify, request
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from datetime import datetime
+
+def register_task_routes(app, db, bcrypt, jwt):
+    # create a task
+    @app.route('/task', methods=['POST'])
+    @jwt_required()
+    def create_task():
+        message = ""
+        res_data = {}
+        code = 500
+        status = "fail"
+        try:
+            data = request.get_json()
+            current_user = get_jwt_identity()
+            teacher = db.teachers.find_one({'username':current_user})
+            title = make_task_title(data['topic'], data['type'])
+            if db.tasks.find_one({'title':title}):
+                message = "task with that name already exists"
+                code = 401
+                status = "fail"
+            else:
+                data['title'] = title
+                data['teacher_id'] = teacher["_id"]
+                data['taskCreated'] = datetime.now()
+                data['is_deleted'] = False
+                res = db.tasks.insert_one(data)
+                if res.acknowledged:
+                    status = "successful"
+                    message = "task created successfully"
+                    code = 200
+                    res_data = {"title": title, "taskCreated": data["taskCreated"]}
+        except Exception as ex:
+            message = f"{ex}"
+            status = "fail"
+            code = 500    
+        return jsonify({'status': status, "data": res_data, "message":message}), code
+    
+    # get all tasks
+    @app.route('/task', methods=['GET'])
+    @jwt_required()
+    def get_tasks():
+        message = ""
+        res_data = {}
+        code = 500
+        status = "fail"
+        try:
+            res = db.tasks.find({"is_deleted": {"$ne": True}})
+            status = "successful"
+            message = "task found"
+            code = 200
+            tasks = list(res)
+            for task in tasks:
+                del task["_id"]
+                del task["teacher_id"]
+            res_data = {"tasks": tasks}
+        except Exception as ex:
+            message = f"{ex}"
+            status = "fail"
+            code = 500    
+        return jsonify({'status': status, "data": res_data, "message":message}), code
+
+    # get a task by title
+    @app.route('/task/<title>', methods=['GET'])
+    @jwt_required()
+    def get_task(title):
+        message = ""
+        res_data = {}
+        code = 500
+        status = "fail"
+        try:
+            task = db.tasks.find_one({'title': title, 'is_deleted': {'$ne': True}})
+            if task:
+                status = "successful"
+                message = "task found"
+                code = 200
+                del task["_id"]
+                del task["teacher_id"]
+
+                # TODO:: return real user
+                task['createdBy'] = 'teacher'
+
+                res_data = task
+            else:                
+                message = "no task found with that name"
+                code = 404
+                status = "fail"
+        except Exception as ex:
+            message = f"{ex}"
+            status = "fail"
+            code = 500    
+        return jsonify({'status': status, "data": res_data, "message":message}), code #adjust return value
+   
+   #edit a task
+    @app.route('/task/<title>', methods=['PUT'])
+    @jwt_required()
+    def edit_task(title):
+        message = ""
+        res_data = {}
+        code = 500
+        status = "fail"
+        try:
+            data = request.get_json()
+            current_user = get_jwt_identity()
+            teacher = db.teachers.find_one({'username':current_user})
+            teacher_id = teacher["_id"]
+            task = db.tasks.find_one({'title':title},{"is_deleted": {"$ne": True}})
+            if task:
+                if  teacher_id != task["teacher_id"]:
+                    message = "you can only edit your own tasks"
+                    code = 403
+                    status = "fail"
+                else:
+                    newvalues = { "$set": { 'difficultyLevel': data.get('difficultyLevel'), 
+                                            'description': data.get('description'), 
+                                            'feedback': data.get('feedback'), 
+                                            'hints': data.get('hints'), 
+                                            'solution': data.get('solution'),
+                                            'output' : data.get('output'), 
+                                            'keywords': data.get('keywords'), 
+                                            'availableLines': data.get('availableLines')
+                                            } }
+                    res = db.tasks.update_one(task, newvalues)
+                    if res.acknowledged:
+                        status = "successful"
+                        message= "edits saved"
+                        code = 200
+                        res_data = {"title": title}
+            else:
+                message = "there is no task with that name"
+                code = 404
+                status = "fail"
+        except Exception as ex:
+            message = f"{ex}"
+            status = "fail"
+            code = 500    
+        return jsonify({'status': status, "data": res_data, "message":message}), code
+    
+    #delete a task
+    @app.route('/task/<title>', methods=['DELETE'])
+    @jwt_required()
+    def delete_task(title):
+        message = ""
+        res_data = {}
+        code = 500
+        status = "fail"
+        try:
+            #verify that the currently logged in teacher is the one who created the task
+            current_user = get_jwt_identity()
+            teacher = db.teachers.find_one({'username':current_user})
+            task = db.tasks.find_one({'title':title})
+            if task:
+                if task['teacher_id'] != teacher["_id"]:
+                    message = "You do not have the authority to delete this task"
+                    code = 403
+                    status = "fail"
+                else:
+                    res = db.tasks.update_one({"title": title}, {"$set": {"is_deleted": True}})
+                    if res.acknowledged:
+                        status = "successful"
+                        message = "task deleted successfully"
+                        code = 200
+                        res_data = {"title": title}
+            else:
+                message = "task not found"
+                code = 404
+                status = "fail"
+        except Exception as ex:
+            message = f"{ex}"
+            status = "fail"
+            code = 500    
+        return jsonify({'status': status, "data": res_data, "message":message}), code
+    
+    @app.route('/generateTaskTitle', methods=['POST'])
+    @jwt_required()
+    def createTitle():
+        message = ""
+        res_data = {}
+        code = 500
+        status = "fail"
+        try:
+            data = request.get_json()
+            topic = data['topic']
+            type = data['type']
+            title = make_task_title(topic, type)
+            status = "successful"
+            message = "title generated successfully"
+            code = 200
+        except Exception as ex:
+            message = f"{ex}, {data}"
+            status = "fail"
+            code = 500    
+        return jsonify({'status': status, "title": title, "message":message}), code
+    
+    def make_task_title(topic, type): 
+        topicCount = db.tasks.count_documents({"topic": topic })
+        typeCount = db.tasks.count_documents({"type": type })
+        title = f"{topic[0]}{topicCount+1}{type[0].upper()}{typeCount + 1}"
+        return title
+    
